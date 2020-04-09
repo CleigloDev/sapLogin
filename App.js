6fa6b939973b3dd3ec4d45a922b65e63965f627c
CleigloDev <carlolune@gmail.com>
2020-04-09 22:24:01 +0200

    add login for cf and neo

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 import WebView from 'react-native-webview';
 import base64 from 'react-native-base64';
@@ -7,7 +7,40 @@ import { sha256 } from 'react-native-sha256';
 const base64js = require('base64-js');
 
 
+const oAuthParams = { // neo params for token
+  client_id: "905fd317-7397-420f-a7c5-e26aff8dc5b8",
+  redirect_uri: "https://oauthasservices-p2000953797trial.hanatrial.ondemand.com",
+  authorize_uri: "https://oauthasservices-p2000953797trial.hanatrial.ondemand.com/oauth2/api/v1/authorize",
+  token_uri: "https://oauthasservices-p2000953797trial.hanatrial.ondemand.com/oauth2/api/v1/token",
+};
+
+const oAuthParamsCF = { // cf params for token
+  client_id: "371b34f5-a548-4083-aad0-bbfdb9f9236b",
+  redirect_uri: "https://p2000953797trial-dev-com-saplogin.cfapps.eu10.hana.ondemand.com",
+  authorize_uri: "https://p2000953797trial-dev-com-saplogin.cfapps.eu10.hana.ondemand.com/oauth2/api/v1/authorize",
+  token_uri: "https://p2000953797trial-dev-com-saplogin.cfapps.eu10.hana.ondemand.com/oauth2/api/v1/token",
+};
+
+let requestPending = false;
+
+
 const MainLogic = () => {
+  const [webViewComp, setWebView] = useState(null);
+  const [verifier, setVerifier] = useState("");
+  const [challenge, setChallange] = useState("");
+
+  useEffect(() => {
+    _generateCodeAndVerifier().then(() => {
+      let vebViewUri = oAuthParams.authorize_uri + "?response_type=code&client_id="+ oAuthParams.client_id + 
+        "&code_challenge=" + challenge + "&code_challenge_method=S256&redirect_uri=" + oAuthParams.redirect_uri;
+      console.log(vebViewUri);
+      let webViewVar = <WebView onNavigationStateChange={_navigationUriChange} source={{uri: vebViewUri}}/>
+      setWebView(webViewVar)
+    })
+    .catch((err) => {
+      alert("Error cannot generate challange and verifier, see logs for more");
+    });
+  }, []);
 
   parseURLParams = (sUrl) => {
     var url = sUrl;
@@ -20,80 +53,76 @@ const MainLogic = () => {
     }
 
     return params;
-  }
+  };
 
   base64URLEncode = (data) => {
+    if (typeof data === "string"){
+      return base64.encode(data).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+    }
     if (data instanceof ArrayBuffer) {
       data = new Uint8Array(data);
     }
     if (data instanceof Uint8Array) {
       const str = base64js.fromByteArray(data);
-      return str
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=/g, '');
+      return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
     }
     if (!ArrayBuffer.isView(data)) {
       throw new Error('data must be ArrayBuffer or typed array');
     }
     const {buffer, byteOffset, byteLength} = data;
     const str = base64js.fromByteArray(new Uint8Array(buffer, byteOffset, byteLength));
-    return str
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=/g, '');
+    return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+  };
+
+  _generateCodeAndVerifier = () => {
+    return new Promise((resolve, reject) => {
+      try{
+        RNSimpleCrypto.utils.randomBytes(32)
+          .then((keyArrayBuffer) => {
+          const verifier = base64URLEncode(keyArrayBuffer);
+          setVerifier(verifier);
+          sha256(verifier).then(hash => {
+            const challenge = base64URLEncode(hash);
+            //base64.encode(hash).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+            setChallange(challenge);
+            resolve();
+          });
+        });
+      }catch(err){
+        console.log(err);
+        reject(err);
+      }
+    });
   }
 
-  _test = async(oEvent) => {
+  _navigationUriChange = (oEvent) => {
     const url = oEvent.url;
+    
+    const params = parseURLParams(url);
 
-// parseURLParams is a pseudo function.
-// Make sure to write your own function or install a package
-RNSimpleCrypto.utils.randomBytes(32)
-.then(async(keyArrayBuffer) => {
-
-const verifier = base64URLEncode(keyArrayBuffer);
-
-console.log(keyArrayBuffer);
-console.log("verifier:" + verifier);
-sha256(verifier).then(hash => {
-  console.log("has256"+hash);
-  const challenge = base64.encode(hash)
-  .replace(/\+/g, '-')
-  .replace(/\//g, '_')
-  .replace(/=/g, '');
-  console.log("challenge:" + challenge);
-})
-//const challange = base64URLEncode(sha256Hash);
-//console.log("challange:" + challange);
-});
-const params = parseURLParams(url);
-let headers = new Headers();
-headers.set('Authorization', 'Basic ' + base64.encode("sb-com-saplogin-dev!t34623" + ":" + "1cGpdkNZytfJSde0oSiSOgzG9lM="));
-console.log("headers"+ headers);
-
-if (params.code) {
-  // Save token for native requests & move to the next screen
-  console.log(params.code);
-
-  fetch("https://p2000953797trial.authentication.eu10.hana.ondemand.com/oauth/token?"+
-    "grant_type=authorization_code&code="+params.code+"&redirect_uri=https://p2000953797trial-dev-com-saplogin.cfapps.eu10.hana.ondemand.com", {method:'GET',
-    headers: headers,
-    //credentials: 'user:passwd'
-   })
-    .then(res => {console.log(res); return res.json()})
-    .then(response => console.log(response))
-    .catch(err => {
-      alert(err);
-    });
-}
-}
+    if (params.code && !requestPending) {
+      requestPending = true;
+      let headers = new Headers();
+      let code = params.code;
+      headers.set('Content-type', 'application/x-www-form-urlencoded');
+      // Save token for native requests & move to the next screen
+      console.log(params.code);
+      let accessTokenUri = oAuthParams.token_uri + "?grant_type=authorization_code&client_id=" + 
+        oAuthParams.client_id + "&code_verifier=" + verifier + "&code=" + code + "&redirect_uri=" + 
+        oAuthParams.redirect_uri;
+      console.log(accessTokenUri);
+      fetch(accessTokenUri, { method: 'Post', headers: headers })
+      .then(res => res.json())
+      .then(response => console.log(response))
+      .catch(err => {
+        alert(err);
+      });
+    }
+  }
 
   return (
     <View style={{flex: 1}}>
-      <WebView 
-       onNavigationStateChange={_test}
-       source={{uri: 'https://p2000953797trial.authentication.eu10.hana.ondemand.com/oauth/authorize?response_type=code&client_id=sb-com-saplogin-dev!t34623&redirect_uri=https://p2000953797trial-dev-com-saplogin.cfapps.eu10.hana.ondemand.com'}}/>
+      {webViewComp}
     </View>
   );
 };
