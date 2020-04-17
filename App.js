11ac00cdfbb5cb4700d89b2a2285d5f15ebc6801
CleigloDev <carlolune@gmail.com>
2020-04-17 20:01:12 +0200

    add working login, logout, registration and session invalidation on cf and neo environment

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, SafeAreaView, AsyncStorage, Button, Platform} from 'react-native';
+import {View, Text, SafeAreaView, AsyncStorage, Button, Platform, TextInput, FlatList} from 'react-native';
 import WebView from 'react-native-webview';
 import base64 from 'react-native-base64';
 import RNSimpleCrypto from "react-native-simple-crypto";
 import { sha256 } from 'react-native-sha256';
 const base64js = require('base64-js');
+import CookieManager from '@react-native-community/cookies';
 
 
 const oAuthParams = { // neo params for token
@@ -15,14 +16,21 @@ const oAuthParams = { // neo params for token
   authorize_uri: "https://oauthasservices-p2000953797trial.hanatrial.ondemand.com/oauth2/api/v1/authorize",
   token_uri: "https://oauthasservices-p2000953797trial.hanatrial.ondemand.com/oauth2/api/v1/token",
   deviceRegistration_path: "/odata/applications/v4/com.loginsap/Connections",
+  logout_uri: "/mobileservices/sessions/logout",
+  logout_method: "POST"
 };
 
 const oAuthParamsCF = { // cf params for token
   client_id: "371b34f5-a548-4083-aad0-bbfdb9f9236b",
+  app_id: "com.saplogin",
+  mobileServiceHost_uri: "https://p2000953797trial-dev-com-saplogin.cfapps.eu10.hana.ondemand.com",
   redirect_uri: "https://p2000953797trial-dev-com-saplogin.cfapps.eu10.hana.ondemand.com",
   authorize_uri: "https://p2000953797trial-dev-com-saplogin.cfapps.eu10.hana.ondemand.com/oauth2/api/v1/authorize",
   token_uri: "https://p2000953797trial-dev-com-saplogin.cfapps.eu10.hana.ondemand.com/oauth2/api/v1/token",
-};
+  deviceRegistration_path: "/odata/applications/v4/com.saplogin/Connections",
+  logout_uri: "/mobileservices/sessions/logout",
+  logout_method: "GET"
+};//collections: Persons, Products, ProductDetail, Suppliers, Categories
 
 let requestPending = false;
 
@@ -33,9 +41,12 @@ const MainLogic = () => {
   const [challenge, setChallange] = useState("");
   const [authInfo, setAuthInfo] = useState(null);
   const [tokenDevice, setTokenDevice] = useState("");
+  const [cookieSession, setCookieSession] = useState("");
+  const [value, onChangeText] = useState("");
+  const [dataArray, setDataArray] = useState([]);
 
   useEffect(() => {
-    _getSessionInfo().then(() => {
+    _getToken().then(() => {
       _requestLogin();
     });
   }, []);
@@ -44,6 +55,7 @@ const MainLogic = () => {
     if(!authInfo){
       _generateCodeAndVerifier().then(() => {
         let vebViewUri = oAuthParams.authorize_uri + "?response_type=code&client_id="+ oAuthParams.client_id + 
+          "&token_format=opaque"+
           "&code_challenge=" + challenge + "&code_challenge_method=S256&redirect_uri=" + oAuthParams.redirect_uri;
         //console.log(vebViewUri);
         let webViewVar = <WebView 
@@ -94,26 +106,45 @@ const MainLogic = () => {
   _generateCodeAndVerifier = () => {
     return new Promise((resolve, reject) => {
       try{
-        //let promiseForByteArray = Platform.OS === "android" ? RNSimpleCrypto.SHA.randomBytes(32) :
-          //RNSimpleCrypto.utils.randomBytes(32);
-        //promiseForByteArray
-        RNSimpleCrypto.SHA.randomBytes(32)
-          .then((keyArrayBuffer) => {
-          const verifier = base64URLEncode(keyArrayBuffer);
-          setVerifier(verifier);
-          sha256(verifier).then(hash => {
-            const challenge = base64URLEncode(hash);
-            //base64.encode(hash).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
-            setChallange(challenge);
-            resolve();
-          });
-        });
+        if(Platform.OS === "android"){
+          _defineChallengeAndroid(resolve);
+        }else{
+          _defineChallengeIos(resolve);
+        }
       }catch(err){
         console.log(err);
         reject(err);
       }
     });
   };
+
+  _defineChallengeAndroid = (fnResolve) => {
+    RNSimpleCrypto.SHA.randomBytes(32)
+      .then((keyArrayBuffer) => {
+      const verifier = base64URLEncode(keyArrayBuffer);
+      setVerifier(verifier);
+      sha256(verifier).then(hash => {
+        const challenge = base64URLEncode(hash);
+        //base64.encode(hash).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+        setChallange(challenge);
+        fnResolve();
+      });
+    });
+  };
+
+  _defineChallengeIos = (fnResolve) => {
+    RNSimpleCrypto.utils.randomBytes(32)
+      .then((keyArrayBuffer) => {
+      const verifier = base64URLEncode(keyArrayBuffer);
+      setVerifier(verifier);
+      sha256(verifier).then(hash => {
+        const challenge = base64URLEncode(hash);
+        //base64.encode(hash).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+        setChallange(challenge);
+        fnResolve();
+      });
+    });
+  }
 
   _navigationUriChange = (oEvent) => {
     const url = oEvent.url;
@@ -137,7 +168,13 @@ const MainLogic = () => {
         console.log(response);
         setAuthInfo(response);
         _saveSessionInfo(response);
-        setWebView(null);
+        //console.log(response);
+        _registerDevice().then(() => {
+          setWebView(null);
+        })
+        .catch(() => {
+          alert("Something went wrong, see console for more info");
+        });
       })
       .catch(err => {
         alert(err);
@@ -158,16 +195,30 @@ const MainLogic = () => {
     }
   };
 
-  _getSessionInfo = async() => {
+  _getToken = async() => {
     try{
       let oAuthInfo = await _getElementFromStorage('sessionInfo');
       if(oAuthInfo !== null) {
         oAuthInfo = JSON.parse(oAuthInfo);
         setAuthInfo(oAuthInfo);
+        console.log(oAuthInfo);
         return oAuthInfo;
       }
     }catch(err){
       alert("Could not retrive auth info");
+      console.log(err);
+    }
+  };
+
+  _getSessionCookie = async() => {
+    try{
+      const sSessionCookie = await _getElementFromStorage('sessionCookie');
+      if(sSessionCookie !== null) {
+        setCookieSession(sSessionCookie);
+        return sSessionCookie;
+      }
+    }catch(err){
+      alert("Could not retrieve session cookie");
       console.log(err);
     }
   };
@@ -184,6 +235,10 @@ const MainLogic = () => {
 
   _saveRegistrationToken = async(sRegistrationToken) => {
     await _saveElementToStorage('registrationToken', sRegistrationToken);
+  };
+
+  _saveSessionCookie = async(sSessionCookie) => {
+    await _saveElementToStorage('sessionCookie', sSessionCookie);
   };
 
   _saveSessionInfo = async(oAuthInfo) => {
@@ -203,67 +258,229 @@ const MainLogic = () => {
   };
 
   _registerDevice = () => {
-    let headers = new Headers();
-    const DeviceType = Platform.OS === "android" ? "Android" : "iPhone";
-    headers.set('Content-type', 'application/xml');
-    headers.set('Authorization', 'Bearer ' + authInfo.access_token);
-    fetch(oAuthParams.mobileServiceHost_uri+oAuthParams.deviceRegistration_path, {
-      method: "POST", headers: headers, body: '<?xml version="1.0" encoding="UTF-8"?><entry xmlns="http://www.w3.org/2005/Atom" xmlns:d="http://schemas.microsoft.com/ado/2007/08/dataservices" xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"><content type="application/xml"><m:properties><d:DeviceType>'+DeviceType+'</d:DeviceType></m:properties></content></entry>'
-    })
-    .then(res => {if(res.status === 201) return res.text(); else return null;})
-    .then(response => {
-        const deviceToken = response ? response.substr(1379+27, response.substr(1379+27).search("<")) : "";
-        deviceToken && deviceToken !== "" ? (
-          setTokenDevice(deviceToken),
-          _saveRegistrationToken(deviceToken).then(() => alert("Registration token saved success")))
-        : null;
-    })
-    .catch(err => {
-      alert("Errore nella registrazione del device");
-      console.log(err);
+    return new Promise((resolve, reject) => {
+      let headers = new Headers();
+      const DeviceType = Platform.OS === "android" ? "Android" : "iPhone";
+      headers.set('Content-type', 'application/json');
+      headers.set('Authorization', 'Bearer ' + authInfo.access_token);
+      const bodyNeoXml = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns="http://www.w3.org/2005/Atom" xmlns:d="http://schemas.microsoft.com/ado/2007/08/dataservices" xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"><content type="application/xml"><m:properties><d:DeviceType>'+DeviceType+'</d:DeviceType></m:properties></content></entry>';
+      const bodyCFJSON = JSON.stringify({DeviceType});
+      fetch(oAuthParams.mobileServiceHost_uri+oAuthParams.deviceRegistration_path, {
+        method: "POST", headers: headers, body: bodyCFJSON,
+        credentials: 'include'
+      })
+      .then(res => {
+        if(res.status === 201) { 
+          return res.text();
+        }else return null;
+      })
+      .then(response => {
+          const deviceToken = response ? _getDeviceTokenFromString(response) : "";
+          deviceToken && deviceToken !== "" ? 
+            _getSessionCookies().then((cookieSession) => {
+              setTokenDevice(deviceToken);
+              setCookieSession(cookieSession);
+              Promise.all([
+                _saveSessionCookie(cookieSession),
+                _saveRegistrationToken(deviceToken)
+              ])
+              .then(() => {alert("Registration token saved success"); resolve()})
+            })
+          : null;
+      })
+      .catch(err => {
+        alert("Errore nella registrazione del device");
+        console.log(err);
+        reject();
+      })
     })
   };
 
+  _getDeviceTokenFromString = (responseText) => {
+    let deviceToken = "";
+    try{
+      const paresedResponse = JSON.parse(responseText);
+      deviceToken = paresedResponse.d.ApplicationConnectionId;
+    }catch(err){
+      const iApplicationIdPos = responseText.search("<d:ApplicationConnectionId>");
+      deviceToken = responseText.substr(iApplicationIdPos+27, responseText.substr(iApplicationIdPos+27).search("<"));
+    }
+
+    return deviceToken;
+  }
+
   _logout = () => {
-    _getRegistrationToken().then((registrationToken) => {
+    Promise.all([
+      _getSessionCookie(),
+      _getRegistrationToken()
+    ])
+    .then(() => {
+      _deleteRegistrationDevice().then(() => {
+        Promise.all([
+          _invalidateSessionCookie(),
+          CookieManager.clearAll()
+        ])
+        .then(() => {
+          //clear every local variables
+          setAuthInfo(null);
+          setTokenDevice("");
+          setCookieSession("");
+
+          //clear every local saved in storage variables
+          _saveRegistrationToken("");
+          _saveSessionCookie("");
+          _saveSessionInfo(null);
+
+          //show login page
+          _requestLogin();
+        })
+        .catch(() => {
+          console.log("Error during logout");
+        });
+      })
+      .catch(() => {
+        console.log("Error during logout");
+      })
+    });
+  };
+
+  _invalidateSessionCookie = () => {
+    return new Promise((resolve, reject) => {
       let headers = new Headers();
+      headers.set('Cookie', cookieSession);
+      const vebViewUri = oAuthParams.mobileServiceHost_uri+oAuthParams.logout_uri;
+      const logoutMethod = oAuthParams.logout_method;
+      fetch(vebViewUri, {method: logoutMethod, headers: headers})
+      .then(res => res.text())
+      .then(response => {
+        resolve();
+      })
+      .catch(err => {
+        alert("Error during logout");
+        console.log(err);
+        reject(err);
+      });
+    });
+  };
+
+  _deleteRegistrationDevice = () => {
+    return new Promise((resolve, reject) => {
+      let headers = new Headers();
+      const registrationToken = tokenDevice;
       headers.set('Authorization', 'Bearer ' + authInfo.access_token);
-      headers.set('Cookie', 'X-SMP-APPCID='+registrationToken);
+      headers.set('Cookie', cookieSession);
       const vebViewUri = oAuthParams.mobileServiceHost_uri+oAuthParams.deviceRegistration_path+"('"+registrationToken+"')";
       fetch(vebViewUri, {method: "DELETE", headers: headers})
       .then(res => res.text())
       .then(response => {
         console.log(response);
-        setAuthInfo(null);
-        _saveRegistrationToken("");
-        _requestLogin();
-        _saveSessionInfo(null);
+        resolve();
       })
       .catch((err) => {
         alert("Error during logout");
         console.log(err);
+        reject(err);
       });
     });
   };
 
-  _testLogout = (event) => {
-    console.log(event);
+  _fetchData = () => {
+    if(value !== ""){
+      let headers = new Headers();
+      headers.set('Authorization', 'Bearer ' + authInfo.access_token);
+      //headers.set('Cookie', cookieSession);
+      const requestUri = oAuthParams.mobileServiceHost_uri+"/"+oAuthParams.app_id+"/"+value+"?$format=json";
+      fetch(requestUri, {
+        method: "GET", headers: headers, 
+        credentials: 'include'
+      })
+      .then(res => res.json())
+      .then(response => {
+        let data = [];
+        if(response?.value?.length > 0){
+          let aKeys = Object.keys(response.value[0]);
+          let sKey0 = aKeys[0];
+          let sKey1 = aKeys[1];
+          data = response.value.map((oItem) => {
+            return Object.assign({}, {key0: oItem[sKey0], key1: oItem[sKey1]});
+          });
+        }else if(response?.d?.results?.length > 0) {
+          let aKeys = Object.keys(response.d.results[0]);
+          let sKey0 = aKeys[1];
+          let sKey1 = aKeys[2];
+          data = response.d.results.map((oItem) => {
+            return Object.assign({}, {key0: oItem[sKey0], key1: oItem[sKey1]});
+          });
+        }else{
+          alert("Err during fetching data");
+          console.log(JSON.stringify(response));
+        }
+        setDataArray(data);
+      })
+      .catch(err => {
+        alert("Err during fetching data");
+        console.log(err);
+      })
+    }
   };
 
+  _getSessionCookies = () => {
+    return new Promise((resolve, reject) => {
+      try{
+        CookieManager.get(oAuthParams.mobileServiceHost_uri)
+        .then((res) => {
+          let cookieString = "";
+          for(let key in res){
+            cookieString += key+"="+res[key].value+"; ";
+          }
+          resolve(cookieString);
+        });
+      }catch(err){
+        reject();
+      }
+    });
+  };
+
+  _renderItem = ({item}) => (
+    <>
+      <Text>{item.key0 + " " + item.key1}</Text>
+    </>
+  );
+
+  /*<>
+    <Text style={{flexShrink: 1}}>{"Token: " + authInfo.access_token}</Text>
+    <Text style={{flexShrink: 1}}>{"TokenType: " + authInfo.token_type}</Text>
+    <Text style={{flexShrink: 1}}>{"Expire: " + authInfo.expires_in}</Text>
+    <Text style={{flexShrink: 1}}>{"RefreshToken: " + authInfo.refresh_token}</Text>
+    <Text style={{flexShrink: 1}}>{"Scopes: " + authInfo.scope}</Text>
+    <Button title="Register Device" onPress={_registerDevice} />
+    <Text style={{flexShrink: 1}}>{"RegistrationToken: " + tokenDevice}</Text>
+    <Button title="Get Device Token" onPress={_getRegistrationToken} />
+    <Button title="Do Logout" onPress={_logout} />
+    <Button title="Get Metadata" onPress={_fetchData} />
+  </>*/
   return (
     <SafeAreaView style={{flex: 1}}>
       {webViewComp}
       {!authInfo ? null :
         <>
-          <Text style={{flexShrink: 1}}>{"Token: " + authInfo.access_token}</Text>
-          <Text style={{flexShrink: 1}}>{"TokenType: " + authInfo.token_type}</Text>
-          <Text style={{flexShrink: 1}}>{"Expire: " + authInfo.expires_in}</Text>
-          <Text style={{flexShrink: 1}}>{"RefreshToken: " + authInfo.refresh_token}</Text>
-          <Text style={{flexShrink: 1}}>{"Scopes: " + authInfo.scope}</Text>
-          <Button title="Register Device" onPress={_registerDevice} />
-          <Text style={{flexShrink: 1}}>{"RegistrationToken: " + tokenDevice}</Text>
-          <Button title="Get Device Token" onPress={_getRegistrationToken} />
-          <Button title="Do Logout" onPress={_logout} />
+          <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 10, borderRadius: 5}}
+            onChangeText={text => onChangeText(text)}
+            placeholder={"Inserisci nome collezione NorthWind"}
+            value={value}
+          />
+          <View style={{flexDirection: "row"}}>
+            <View style={{paddingRight: "70%"}}>
+              <Button title="Logout" onPress={_logout} />
+            </View>
+            <Button title="GO!" onPress={_fetchData} />
+          </View>
+            <FlatList
+              data={dataArray}
+              renderItem={this._renderItem}
+              extraData={dataArray}
+            />
         </>
       }
     </SafeAreaView>
